e7f9baa0ab562e16fc3f8033f77c9d68f37acc46
Eder Rodriguez <edrod@microsoft.com>
2021-03-24 17:52:06 -0600

    Gesture Minigame cleanup

--- GamifiedInputApp/GamifiedInputApp/GamifiedInputApp/Minigames/Gesture/GestureRecognizerMinigame.cs
+++ GamifiedInputApp/GamifiedInputApp/GamifiedInputApp/Minigames/Gesture/GestureRecognizerMinigame.cs
@@ -1,6 +1,9 @@
 using Microsoft.UI.Composition;
 using Microsoft.UI.Composition.Experimental;
-using Microsoft.UI.Input.Experimental; 
+using Microsoft.UI.Input.Experimental;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +16,14 @@ namespace GamifiedInputApp.Minigames.Gesture
 {
     class GestureRecognizerMinigame : IMinigame
     {
+        // UI Components
+        private Window window;
+        private ContainerVisual rootVisual; 
+        private SpriteVisual sprite; // TODO: Might need a list of these ?
+
         // Input API
         private ExpPointerInputObserver pointerInputObserver; 
         private ExpGestureRecognizer gestureRecognizer;
-
-        private SpriteVisual sprite;
 
         // Minigame variables
         private const int TOTAL_TAPS_TO_WIN = 15; 
@@ -27,7 +33,7 @@ namespace GamifiedInputApp.Minigames.Gesture
 
         public void End(in GameContext gameContext, in MinigameState finalState)
         {
-            return; 
+            window.Close();
         }
 
         public void Start(in GameContext gameContext, ContainerVisual rootVisual, ExpInputSite inputSite)
@@ -51,33 +57,6 @@ namespace GamifiedInputApp.Minigames.Gesture
             return result; 
         }
 
-        public void Setup(ContainerVisual rootVisual)
-        {
-            tapCounter = 0;
-
-            // Generate visual for tap game.
-            Compositor compositor = rootVisual.Compositor;
-            sprite = compositor.CreateSpriteVisual();
-            sprite.Brush = compositor.CreateColorBrush(Windows.UI.Color.FromArgb(0xFF, 0x00, 0xB0, 0xF0));
-            sprite.Size = new Vector2(100, 100);
-
-            // Create InputSite
-            var content = ExpCompositionContent.Create(compositor);
-            var inputsite = ExpInputSite.GetOrCreateForContent(content);
-
-            // PointerInputObserver
-            pointerInputObserver = ExpPointerInputObserver.CreateForInputSite(inputsite);
-            pointerInputObserver.PointerPressed += OnPointerPressed;
-            pointerInputObserver.PointerReleased += OnPointerReleased;
-
-            // GestureRecognizer
-            gestureRecognizer = new ExpGestureRecognizer();
-            gestureRecognizer.GestureSettings = Windows.UI.Input.GestureSettings.Tap;
-            gestureRecognizer.Tapped += Tapped;
-
-            rootVisual.Children.InsertAtTop(sprite);
-        }
-
         //
         // Event Handlers 
         //
@@ -97,6 +76,55 @@ namespace GamifiedInputApp.Minigames.Gesture
         private void Tapped(object sender, ExpTappedEventArgs eventArgs)
         {
             ++tapCounter;  
+        }
+
+        // 
+        // Helper Functions
+        //
+
+        private void Setup(ContainerVisual rootVisual)
+        {
+            tapCounter = 0;
+
+            this.UISetup();
+            this.InputAPISetup(); 
+        }
+
+        private void UISetup()
+        {
+            window = new Window();
+
+            var compositor = window.Compositor;
+            rootVisual = compositor.CreateContainerVisual();
+
+            UIElement rootElement = new Grid();
+            ElementCompositionPreview.SetElementChildVisual(rootElement, rootVisual);
+
+            sprite = compositor.CreateSpriteVisual();
+            sprite.Brush = compositor.CreateColorBrush(Windows.UI.Color.FromArgb(0xFF, 0x00, 0xB0, 0xF0));
+            sprite.Size = new Vector2(100, 100);
+
+            rootVisual.Children.InsertAtTop(sprite);
+            window.Activate(); 
+        }
+
+        private void InputAPISetup()
+        {
+            var compositor = sprite.Compositor; 
+
+            // Create InputSite
+            var content = ExpCompositionContent.Create(compositor);
+            var inputsite = ExpInputSite.GetOrCreateForContent(content);
+
+            // PointerInputObserver
+            pointerInputObserver = ExpPointerInputObserver.CreateForInputSite(inputsite);
+            pointerInputObserver.PointerPressed += OnPointerPressed;
+            pointerInputObserver.PointerReleased += OnPointerReleased;
+
+            // GestureRecognizer
+            gestureRecognizer = new ExpGestureRecognizer();
+            gestureRecognizer.GestureSettings = Windows.UI.Input.GestureSettings.Tap;
+            gestureRecognizer.Tapped += Tapped;
         }
     }
 }
