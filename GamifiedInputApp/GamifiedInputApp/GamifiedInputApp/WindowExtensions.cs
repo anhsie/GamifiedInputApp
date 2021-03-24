52e2445c4d99327a4adcafebdd34879f2b085c8f
Anthony Hsieh <anhsie@microsoft.com>
2021-03-24 12:11:30 -0700

    added cswin32 generator

---
+++ GamifiedInputApp/GamifiedInputApp/GamifiedInputApp/WindowExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using WinRT;
+
+namespace GamifiedInputApp
+{
+    public static class WindowExtensions
+    {
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
+        }
+
+        public static IntPtr GetWindowHandle(this Microsoft.UI.Xaml.Window window)
+            => window is null ? throw new ArgumentNullException(nameof(window)) : window.As<IWindowNative>().WindowHandle;
+    }
+}
