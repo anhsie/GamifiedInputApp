9ac2d4e60e79bcbd830cc1aaefaefccc6850e36c
Justin Nordin <jnordi@microsoft.com>
2021-03-26 12:19:23 -0700

    Ink analysis working! Now I just need to hook it up to make the actual minigame work.

--- GamifiedInputApp/GamifiedInputApp/GamifiedInputApp/Minigames/Handwriting/HandwritingMinigame.cs
+++ GamifiedInputApp/GamifiedInputApp/GamifiedInputApp/Minigames/Handwriting/HandwritingMinigame.cs
@@ -3,7 +3,10 @@ using Microsoft.UI.Input.Experimental;
 using Microsoft.UI.Xaml.Media;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Numerics;
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
 
 namespace GamifiedInputApp.Minigames.Handwriting
 {
@@ -21,6 +24,8 @@ namespace GamifiedInputApp.Minigames.Handwriting
         int m_nextEllipse = 0;
         List<ShapeVisual> m_ellipseVisuals;
         ExpPointerInputObserver m_pointerInput;
+        InkAnalyzer m_inkAnalyzer;
+        InkStrokeBuilder m_inkStrokebuilder;
 
         public HandwritingMinigame()
         {
@@ -34,6 +39,8 @@ namespace GamifiedInputApp.Minigames.Handwriting
 
             m_random = new Random();
             m_letterIndex = m_random.Next(m_letterImages.Count);
+            m_inkAnalyzer = new InkAnalyzer();
+            m_inkStrokebuilder = new InkStrokeBuilder();
         }
 
         public void Start(in GameContext gameContext, ContentHelper contentHelper)
@@ -72,6 +79,9 @@ namespace GamifiedInputApp.Minigames.Handwriting
             m_pointerInput.PointerMoved += M_pointerInput_PointerMoved;
             m_pointerInput.PointerReleased += M_pointerInput_PointerReleased;
 
+            var inkAnalyzer = new Windows.UI.Input.Inking.Analysis.InkAnalyzer();
+            var strokebuilder = new Windows.UI.Input.Inking.InkStrokeBuilder();
+
             UpdateLayout();
         }
 
@@ -93,7 +103,60 @@ namespace GamifiedInputApp.Minigames.Handwriting
 
         void M_pointerInput_PointerReleased(ExpPointerInputObserver sender, ExpPointerEventArgs args)
         {
-            //...
+            if (m_inkAnalyzer.IsAnalyzing)
+            {
+                return;
+            }
+
+            List<Windows.Foundation.Point> points = new List<Windows.Foundation.Point>(m_ellipseVisuals.Count);
+
+            int currentEllipse = m_nextEllipse;
+            for (int strokesInspected = 0; strokesInspected < m_ellipseVisuals.Count; strokesInspected++)
+            {
+                if (m_ellipseVisuals[currentEllipse].IsVisible)
+                {
+                    points.Add(new Windows.Foundation.Point(
+                        m_ellipseVisuals[currentEllipse].Offset.X,
+                        m_ellipseVisuals[currentEllipse].Offset.Y));
+                }
+
+                currentEllipse++;
+                if (currentEllipse >= m_ellipseVisuals.Count)
+                {
+                    currentEllipse = 0;
+                }
+            }
+
+            if (points.Count > 0)
+            {
+                m_inkAnalyzer.ClearDataForAllStrokes();
+                InkStroke stroke = m_inkStrokebuilder.CreateStroke(points);
+                m_inkAnalyzer.AddDataForStroke(stroke);
+                m_inkAnalyzer.SetStrokeDataKind(stroke.Id, InkAnalysisStrokeKind.Writing);
+                var operation = m_inkAnalyzer.AnalyzeAsync();
+                operation.Completed = (result, status) => 
+                {
+                    Debugger.Log(1, "Ink Analysis Result", string.Format("Status = {0}", status));
+                    if (status == Windows.Foundation.AsyncStatus.Completed)
+                    {
+                        InkAnalysisResult analysisResult = result.GetResults();                        
+                        if (analysisResult.Status == InkAnalysisStatus.Updated)
+                        {
+                            // Find all strokes that are recognized as handwriting and 
+                            // create a corresponding ink analysis InkWord node.
+                            var inkwordNodes =
+                                m_inkAnalyzer.AnalysisRoot.FindNodes(
+                                    InkAnalysisNodeKind.InkWord);
+
+                            // Iterate through each InkWord node.
+                            foreach (InkAnalysisInkWord node in inkwordNodes)
+                            {
+                                Debugger.Log(1, "Ink Analysis Result", string.Format("Word Recognized = '{0}'", node.RecognizedText));
+                            }
+                        }
+                    }
+                };
+            }
         }
 
         void Content_StateChanged(Microsoft.UI.Composition.Experimental.ExpCompositionContent sender, Microsoft.UI.Composition.Experimental.ExpCompositionContentEventArgs args)
